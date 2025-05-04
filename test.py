63cde80a1d1e5f2e389dc659e8c590a2e5997d56
jaygupta907 <ae21b026@smail.iitm.ac.in>
2025-05-04 09:36:28 -0700

    Added Requirements.txt

--- test.py
+++ test.py
@@ -9,23 +9,23 @@ from dataset import CharVocab
 input_vocab = CharVocab.load("saved/input_vocab.pt")
 output_vocab = CharVocab.load("saved/output_vocab.pt")
 
-encoder = Encoder(len(input_vocab), embed_dim=512, hidden_dim=512, num_layers=4, rnn_type='LSTM')
-decoder = Decoder(len(output_vocab), embed_dim=512, hidden_dim=512, num_layers=4, rnn_type='LSTM',use_attention=False,encoder_output_dim=1024)
+encoder = Encoder(len(input_vocab), embed_dim=512, hidden_dim=512, num_layers=2, rnn_type='LSTM')
+decoder = Decoder(len(output_vocab), embed_dim=512, hidden_dim=512, num_layers=2, rnn_type='LSTM',use_attention=True,encoder_output_dim=1024)
 model = Seq2Seq(encoder, decoder, rnn_type='LSTM').to(DEVICE)
-model.load_state_dict(torch.load("saved/seq2seq_model_vanilla.pt", map_location=DEVICE))
+model.load_state_dict(torch.load("saved/seq2seq_model_attention.pt", map_location=DEVICE))
 model.eval()
 
 def translate_word(word):
     input_tensor = input_vocab.tensor_from_text(word).to(DEVICE)
 
     with torch.no_grad():
-        output= model.translate(input_tensor,output_vocab, max_len=30)
+        output,_= model.translate(input_tensor,output_vocab, max_len=30)
         output_word = output_vocab.decode(output)
         
     return output_word
 
 path = test_path = "datasets/dakshina_dataset_v1.0/hi/lexicons/hi.translit.sampled.test.tsv"
-output_file = "prediction_vanilla.tsv"
+output_file = "prediction_attention.tsv"
 with open(path, 'r', encoding='utf8') as f,open(output_file, 'w', encoding='utf8') as out_f:
     for line in f:
         parts = line.strip().split('\t')
