ccbb85cfb8f61bb1331f90a8d46d34d118c9e324
jaygupta907 <ae21b026@smail.iitm.ac.in>
2025-05-18 02:41:03 -0700

    Added sweep

--- train.py
+++ train.py
@@ -6,6 +6,12 @@ from model import Encoder, Decoder, Seq2Seq
 import logging
 from tqdm import tqdm
 import os
+import argparse
+from config import get_args
+import wandb
+
+
+import os
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -69,6 +75,13 @@ def evaluate(model, dataloader, criterion):
     return epoch_loss / len(dataloader), epoch_accuracy / len(dataloader)
 
 def main():
+    args = get_args()
+
+
+    wandb.init(project=args.wandb_project, entity=args.wandb_entity)
+    wandb.config.update(args)
+
+
     train_path = "datasets/dakshina_dataset_v1.0/hi/lexicons/hi.translit.sampled.train.tsv"
     dev_path = "datasets/dakshina_dataset_v1.0/hi/lexicons/hi.translit.sampled.dev.tsv"
     test_path = "datasets/dakshina_dataset_v1.0/hi/lexicons/hi.translit.sampled.test.tsv"
@@ -80,42 +93,52 @@ def main():
     dev_dataset = DakshinaDataset(dev_path, input_vocab=input_vocab, output_vocab=output_vocab)
     test_dataset = DakshinaDataset(test_path, input_vocab=input_vocab, output_vocab=output_vocab)
 
-    train_loader = DataLoader(train_dataset, batch_size=256, shuffle=True, collate_fn=collate_fn)
-    dev_loader = DataLoader(dev_dataset, batch_size=256, shuffle=False, collate_fn=collate_fn)
-    test_loader = DataLoader(test_dataset, batch_size=256, shuffle=False, collate_fn=collate_fn)
+    train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, collate_fn=collate_fn)
+    dev_loader = DataLoader(dev_dataset, batch_size=args.batch_size, shuffle=False, collate_fn=collate_fn)
+    test_loader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False, collate_fn=collate_fn)
 
     input_vocab_size = len(train_dataset.input_vocab)
     output_vocab_size = len(train_dataset.output_vocab)
 
-    encoder = Encoder(input_vocab_size, embed_dim=512, hidden_dim=512, num_layers=2, rnn_type='LSTM')
-    decoder = Decoder(output_vocab_size, embed_dim=512, hidden_dim=512, num_layers=2, rnn_type='LSTM',use_attention=True,encoder_output_dim=1024)
-    model = Seq2Seq(encoder, decoder, rnn_type='LSTM').to(DEVICE)
-
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+    encoder = Encoder(input_vocab_size, 
+                    embed_dim=args.input_embedding,
+                    hidden_dim=args.hidden_layer_size,
+                    num_layers=args.encoder_layers, 
+                    rnn_type=args.cell_type,
+                    bidirectional=args.encoder_bidirectional, 
+                    dropout=args.dropout)
+    decoder = Decoder(output_vocab_size, 
+                      embed_dim=args.input_embedding, 
+                      hidden_dim=args.hidden_layer_size, 
+                      num_layers=args.decoder_layers, 
+                      rnn_type=args.cell_type,
+                      bidirectional=args.decoder_bidirectional,
+                      dropout=args.dropout,
+                      use_attention=args.use_attention,
+                      encoder_output_dim=args.hidden_layer_size * 2 if args.encoder_bidirectional else args.hidden_layer_size)
+    model = Seq2Seq(encoder, decoder, rnn_type=args.cell_type).to(DEVICE)
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate)
     criterion = nn.CrossEntropyLoss(ignore_index=0)
 
-    N_EPOCHS = 20
     CLIP = 1
 
-    for epoch in range(N_EPOCHS):
+    for epoch in range(args.num_epochs):
 
-        if epoch < 5:
-            tf_ratio = 1
-        else:
-            tf_ratio = 0
-        logging.info(f"Epoch {epoch+1}/{N_EPOCHS} | Teacher Forcing Ratio: {tf_ratio}")
+        tf_ratio = 0
+        logging.info(f"Epoch {epoch+1}/{args.num_epochs} | Teacher Forcing Ratio: {tf_ratio}")
         train_loss, train_accuracy = train_one_epoch(model, train_loader, optimizer, criterion, CLIP, tf_ratio)
+        wandb.log({"train_loss": train_loss, "train_accuracy": train_accuracy})
         print("=" * os.get_terminal_size().columns)
         dev_loss, dev_accuracy = evaluate(model, dev_loader, criterion)
-
+        wandb.log({"val_loss": dev_loss, "val_accuracy": dev_accuracy})
         logging.info(f"Train Loss: {train_loss:.4f}, Train Accuracy: {train_accuracy:.4f}, Dev Loss: {dev_loss:.4f}, Dev Accuracy: {dev_accuracy:.4f}")
 
-    test_loss, test_accuracy = evaluate(model, test_loader, criterion)
+
     # Save the model and vocabularies
-    os.makedirs("saved", exist_ok=True)
-    torch.save(model.state_dict(), "saved/seq2seq_model_attention.pt")
-    torch.save(train_dataset.input_vocab.__dict__, "saved/input_vocab.pt")
-    torch.save(train_dataset.output_vocab.__dict__, "saved/output_vocab.pt")
+    # os.makedirs("saved", exist_ok=True)
+    # torch.save(model.state_dict(), "saved/seq2seq_model_attention.pt")
+    # torch.save(train_dataset.input_vocab.__dict__, "saved/input_vocab.pt")
+    # torch.save(train_dataset.output_vocab.__dict__, "saved/output_vocab.pt")
 
 if __name__ == '__main__':
     main()
